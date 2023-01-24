4334a4fe1381041249cc01db4cf4ec9d333f5e26
SinaSS77 <s.molaverdikhani>
2023-01-23 17:05:02 -0800

    added EJS template, created views subdirectory, sended a variable to the template

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set('view engin', 'ejs');
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("This is our home page, Welcome!");
@@ -20,6 +25,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send(`<html><body><h1>This is our home page</h1><br><h3><i>Welcome!</i></h3></body></html>\n`);
 });
+
+
 
 app.listen(PORT, (req, res) =>{
   console.log(`The port is : ${PORT}`);
