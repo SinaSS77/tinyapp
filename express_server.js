13a7217a923cd6d8fed7ffbedc76ca7650568f4f
SinaSS77 <s.molaverdikhani>
2023-01-24 00:12:46 -0800

    end of day one redirecting to the show page and to the original page

--- express_server.js
+++ express_server.js
@@ -1,33 +1,42 @@
+// Using the Express after installing it by npm
 const express = require('express');
 
 const app = express();
+//defining the port
 const PORT = 8080;
 
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true })); //will translate, or parse the body  and make it readable for us humans. This feature is part of Express.
+app.set("view engine", "ejs"); //for using the templates in the views folder after insalling the EJS by npm
 
+//Temporary using an object instead of database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const generateRandomString = function() {};
+// a function to generate 6length random string as our shortURL (id)
+const generateRandomString = function() {
+  let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+};
 
+/***********   ROUTS */
+/******************* */
 
+//getting any responce to route urls and rendering by ejs
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//getting any responce to route urls/news and rendering by ejs (for the submition page)
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
-
 
 
 app.get("/", (req, res) => {
@@ -42,9 +51,31 @@ app.get("/hello", (req, res) => {
   res.send(`<html><body><h1>This is our home page</h1><br><h3><i>Welcome!</i></h3></body></html>\n`);
 });
 
+// this is for redirecting from POST route to this page to show the user the short links
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+// inside the urls_show.ejs it is defined in the anchor link that by click on the shortURLs it should go to the /u/:id. and inside of this we define that it should redirect to the longURL
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+
+/***********   POSTS */
+/******************* */
+
+//by clicking on the Submit button we use from these things :action, name and method to post our reqest and implement functions and store the shortURL and redirect to /urls/:id
 app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log(shortURL);
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, (req, res) =>{
