445084752a55451738ff234dad7a9e6ca039a939
SinaSS77 <s.molaverdikhani>
2023-01-25 22:15:40 -0800

    make a function files to keep the code wet

---
+++ Functions.js
@@ -0,0 +1,49 @@
+
+const urlsForUser = (id, urlDatabase) => {
+  let userURLdata = {};
+  for (const url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      userURLdata[url] = urlDatabase[url];
+    }
+  }
+  return userURLdata;
+};
+
+//create a random 6 character string for short url
+const getRandomString = () => {
+  let randomCharsStr = '';
+  const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    randomCharsStr += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return randomCharsStr;
+};
+
+//create a function to look up if email already exist
+const emailExists = (emailAddress, users) => { // james justs added users
+  for (const user in users) {
+    if (emailAddress === users[user].email) {
+      return true;
+    }
+  }
+};
+
+const getUserById = (id, users) => { // returns an object
+  const user = users[id];
+  if (user) {
+    return user;
+  }
+  return null;
+};
+
+const getUserByEmail = (email, users) => {
+  for (const userID in users) {
+    const user = users[userID];
+    if (email === user.email) {
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = { getUserByEmail, getUserById, urlsForUser, emailExists, getRandomString }; 
